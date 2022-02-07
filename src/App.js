4561316710903c237bed7f39555b3d10e3f65503
SD-Jonatha <thiago.infortec@gmail.com>
2022-02-07 16:25:55 -0300

    build test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          New Covid
+          STATISTIC  COVID
         </a>
       </header>
     </div>
